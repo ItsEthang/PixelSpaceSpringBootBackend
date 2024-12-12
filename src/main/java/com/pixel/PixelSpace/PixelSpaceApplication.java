828a386dbed75952e52f0d985e35f2ee434b5fed
Ethan Tang <itsethang5@gmail.com>
2024-12-11 20:29:13 -0500

    init commit

---
+++ src/main/java/com/pixel/PixelSpace/PixelSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.pixel.PixelSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixel/PixelSpace/PixelSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixel.PixelSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
