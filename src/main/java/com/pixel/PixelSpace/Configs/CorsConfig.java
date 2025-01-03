4d97a5d1659b5ee0f815b4a311abbf492605f812
Ethan Tang <itsethang5@gmail.com>
2025-01-03 11:20:05 -0500

    update CORS config for ec2 instance

--- src/main/java/com/pixel/PixelSpace/Configs/CorsConfig.java
+++ src/main/java/com/pixel/PixelSpace/Configs/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:4173")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:4173",
+                        "http://54.204.148.235")
                 .allowedMethods("GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("userId")
